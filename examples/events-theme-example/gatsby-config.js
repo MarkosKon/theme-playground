673aed65cd504494fa939d573af323453f89a689
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-07-05 17:26:02 +0300

    Using the gatsby-theme-ui in the events example.

--- examples/events-theme-example/gatsby-config.js
+++ examples/events-theme-example/gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   plugins: [
+    "gatsby-theme-ui",
     {
       resolve: "gatsby-theme-events",
       options: {
---
+++ examples/events-theme-example/src/gatsby-theme-events/components/Layout.jsx
@@ -0,0 +1,3 @@
+import Layout from "gatsby-theme-ui/src/components/Layout";
+
+export default Layout;
