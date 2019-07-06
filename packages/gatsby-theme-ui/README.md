ddbac6bd7558b3742718e22ec6022c8724fd8390
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-07-06 11:38:36 +0300

    We now create the directories in the theme-ui theme if they don't exist. We also use the default image if the user does not specify one.

---
+++ packages/gatsby-theme-ui/README.md
@@ -0,0 +1,9 @@
+# gatsby-theme-ui
+
+## Theme options
+
+| Name           | Description                                                 | Default           |
+| -------------- | ----------------------------------------------------------- | ----------------- |
+| imageDirectory | Images for sharp                                            | `src/images`      |
+| pageDirectory  | This exists to make mdx files available to GraphQL queries. | `src/pages`       |
+| logoFilename   | Place the logo inside the `imageDirectory`.                 | `gatsby-icon.png` |
--- packages/gatsby-theme-ui/gatsby-config.js
+++ packages/gatsby-theme-ui/gatsby-config.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 const remarkSlug = require("remark-slug");
 
@@ -8,60 +9,69 @@ const siteMetadata = {
   description: `Gatsby theme ui`,
   author: `Mark`
 };
-module.exports = {
-  siteMetadata,
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-webpack-size`,
-    `gatsby-plugin-emotion`,
-    "gatsby-plugin-catch-links",
-    {
-      resolve: "gatsby-mdx",
-      options: {
-        defaultLayouts: {
-          // posts: require.resolve("./src/components/posts-layout.js"),
-          default: require.resolve("./src/components/Layout.jsx")
-        },
-        extensions: [".mdx", ".md"],
-        remarkPlugins,
-        gatsbyRemarkPlugins: ["gatsby-remark-prismjs"]
+
+module.exports = ({
+  imageDirectory = "src/images",
+  pageDirectory = "src/pages",
+  logoFilename = "gatsby-icon.png"
+}) => {
+  const logoUserPath = path.join(imageDirectory, logoFilename);
+  const logoPath = fs.existsSync(logoUserPath)
+    ? logoUserPath
+    : require.resolve("./src/images/gatsby-icon.png");
+  return {
+    siteMetadata,
+    plugins: [
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-webpack-size`,
+      `gatsby-plugin-emotion`,
+      "gatsby-plugin-catch-links",
+      {
+        resolve: "gatsby-mdx",
+        options: {
+          defaultLayouts: {
+            // posts: require.resolve("./src/components/posts-layout.js"),
+            default: require.resolve("./src/components/Layout.jsx")
+          },
+          extensions: [".mdx", ".md"],
+          remarkPlugins,
+          gatsbyRemarkPlugins: ["gatsby-remark-prismjs"]
+        }
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `mdxPages`,
+          path: path.resolve(`./${pageDirectory}`)
+        }
+      },
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `images`,
+          // Find user's images.
+          path: path.resolve(`./${imageDirectory}`)
+          // To find theme's images:
+          // path: `${__dirname}/src/images`
+        }
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+        resolve: `gatsby-plugin-manifest`,
+        options: {
+          name: siteMetadata.name,
+          short_name: `theme-ui`,
+          start_url: `/`,
+          background_color: `#663399`,
+          theme_color: `#663399`,
+          display: `minimal-ui`,
+          icon: logoPath
+        }
       }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: path.resolve(`./src/pages`)
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        // Find user's images.
-        // You could add a preBootstrap lifecycle to
-        // create the directory if it doesn't exist.
-        path: path.resolve(`./src/images`)
-        // This find theme's images.
-        // path: `${__dirname}/src/images`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: siteMetadata.name,
-        short_name: `theme-ui`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-      }
-    }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ]
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      // `gatsby-plugin-offline`,
+    ]
+  };
 };
