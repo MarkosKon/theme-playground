f2d7a3039a3c29365dcb32317ce275d34975b964
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-12-20 21:04:39 +0200

    Added an example in theme spaces for rem based spaces.

--- packages/gatsby-theme-ui/src/theme/space.js
+++ packages/gatsby-theme-ui/src/theme/space.js
@@ -1,1 +1,18 @@
 export default [0, 4, 8, 16, 32, 64, 96, 128, 256, 512];
+// If you set-up the html, body to be equal to the `root`
+// font size, you can use `rem` based spaces. This way,
+// if you change the font size in mobile, you won't use
+// the spaces for desktop and, as a result, break the rhythm.
+// The following assumes that your line height for body text is 1.5.
+// export default [
+//   0,
+//   "0.1875rem",
+//   "0.375rem",
+//   "0.75rem",
+//   "1.5rem",
+//   "3rem",
+//   "4.5rem",
+//   "6rem",
+//   "12rem",
+//   "24rem"
+// ];
