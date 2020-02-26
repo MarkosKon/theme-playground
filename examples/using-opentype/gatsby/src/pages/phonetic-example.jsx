2d3ff7648d00503297edc0ca19e69931b71882bc
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2020-02-26 20:31:19 +0200

    Minor changes in the opetype package. Also added an example project for gatsby.

---
+++ examples/using-opentype/gatsby/src/pages/phonetic-example.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default () => (
+  <>
+    <main>
+      <h1>Phonetic example</h1>
+      <p>
+        <a href="https://en.wikipedia.org/wiki/Help:IPA">
+          https://en.wikipedia.org/wiki/Help:IPA
+        </a>
+      </p>
+      <h2>Brackets</h2>
+      <p>
+        Two types of brackets are commonly used to enclose transcriptions in the
+        <abbr>IPA</abbr>:
+      </p>
+      <ul>
+        <li>
+          /Slashes/ indicate sounds that are distinguished as the basic units of
+          words in a language by native speakers; these are called phonemes.
+          Changing the symbols between these slashes would either change the
+          identity of the word or produce nonsense. For example, since there is
+          no meaningful difference to a native speaker between the two sounds
+          written with the letter L in the word lulls, they are considered the
+          same phoneme, and so, using slashes, they are given the same symbol in
+          IPA: /ˈlʌlz/. Similarly, Spanish la bamba is transcribed phonemically
+          with two instances of the same b sound, /la ˈbamba/, despite the fact
+          that they sound different to a speaker of English. Thus a reader who
+          is not familiar with the language in question might not know how to
+          interpret these transcriptions more narrowly.
+        </li>
+        <li>
+          [Square brackets] indicate the narrower or more detailed phonetic
+          qualities of a pronunciation, not taking into account the norms of the
+          language to which it belongs; therefore, such transcriptions do not
+          regard whether subtly different sounds in the pronunciation are
+          actually noticeable or distinguishable to a native speaker of the
+          language. Within square brackets is what a foreigner who does not know
+          the structure of a language might hear as discrete units of sound. For
+          instance, the English word lulls may be pronounced in a particular
+          dialect more specifically as [ˈlɐɫz], with different letter L sounds
+          at the beginning and end. This may be obvious to speakers of languages
+          that differentiate between the sounds [l] and [ɫ]. Likewise, Spanish
+          la bamba (pronounced without a pause) has two different b-sounds to
+          the ears of foreigners or linguists—[la ˈβamba]—though a native
+          Spanish speaker might not be able to hear it. Omitting or adding such
+          detail does not make a difference to the identity of the word, but
+          helps to give a more precise pronunciation.
+        </li>
+      </ul>
+      <p>A third kind of bracket is occasionally seen:</p>
+      <ul>
+        <li>
+          Either //double slashes// or |pipes| (or occasionally other
+          conventions) show that the enclosed sounds are theoretical constructs
+          that are not actually heard. (This is part of morphophonology.) For
+          instance, most phonologists argue that the -s at the ends of verbs,
+          which surfaces as either /s/ in talks /tɔːks/ or as /z/ in lulls
+          /lʌlz/, has a single underlying form. If they decide this form is an
+          s, they would write it //s// (or |s|) to claim that phonemic /tɔːks/
+          and /lʌlz/ are essentially //tɔːks// and //lʌls// underneath. If they
+          were to decide it was essentially the latter, //z//, they would
+          transcribe these words //tɔːkz// and //lʌlz//.
+        </li>
+      </ul>
+    </main>
+  </>
+);
