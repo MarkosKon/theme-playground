8bc002d61bea23564d4f7d4855b37efddfd688de
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2020-02-26 16:05:19 +0200

    Added an example that uses the source serif subset package.

---
+++ examples/using-source-serif-subsets/gatsby-browser.js
@@ -0,0 +1,4 @@
+import "@affectionatedoor/opentype";
+import "@affectionatedoor/source-serif-subsets";
+import "@affectionatedoor/source-serif-subsets/variable.css";
+import "./src/styles/main.css";
